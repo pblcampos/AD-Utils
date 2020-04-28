cccc78adea8867fbcc5d2b47e617e4131208a7f6
pblcampos <64409894+pblcampos@users.noreply.github.com>
2020-04-28 12:44:55 +0200

    Add files via upload

---
+++ ClearSpooler.ps1
@@ -0,0 +1,26 @@
+﻿<#
+ .SYNOPSIS
+ Reinicia la cola de impresión
+
+ .DESCRIPTION
+ Script que para el servicio de cola de impresión, borra los archivos pendientes de imprimir de C:\Windows\system32\spool\PRINTERS y vuelve a arrancar el servicio
+
+ .PARAMETER machine
+ Nombre de la maquina en la reiniciar el servicio
+
+ .EXAMPLE
+ Clearspooler.ps1 ZOCHMGU0000
+ #>
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True,Position=0)]
+    [string]$machine
+    )
+    
+    if (Test-NetConnection $machine -InformationLevel Quiet)
+    {
+    (Get-Service -ComputerName $machine -Name Spooler).Stop()
+    Get-ChildItem \\$machine\C$\Windows\System32\spool\PRINTERS | foreach ($_) {remove-item $_.fullname}
+    (Get-Service -ComputerName $machine -Name Spooler).Start()
+    }
